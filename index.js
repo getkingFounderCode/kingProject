24bd66c05dac2491b777a578bde13971c5ea606c
Daniel Onwuka <danielofkd@gmail.com>
2025-04-27 20:37:07 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-bar').onClick=()=>{
+    navbar.classList.toggle('active');
+}
+
+
+
+
+let search = document.querySelector('.search');
+document.querySelector('#search').onClick=()=>{
+    search.classList.toggle('active');
+}
